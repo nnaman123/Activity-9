a46fd88c896056a07275114b2d0be5971afe09cb
Namo18-web-sudo <nebula.foundation.official@gmail.com>
2025-08-05 20:44:09 +0530

    first commit

---
+++ Project-1/main.py
@@ -0,0 +1,8 @@
+#sum of natural numbers number till the number we write
+v = int(input("Enter a number: "))
+sum = 0
+i = 1
+while i <= v:
+    sum += i
+    i += 1
+print("Sum of natural numbers is:", sum)
